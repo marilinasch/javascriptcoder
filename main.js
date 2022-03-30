85e2c0548c389392b299462770fcaf782c8264c7
marilinasch <93880107+marilinasch@users.noreply.github.com>
2022-03-29 23:43:41 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+for (i = 0; i < 2; i++) {
+let zona = prompt("Ingresa zona de envío (1 CABA, 2 ZONA SUR, 3 ZONA NORTE, 4 ZONA OESTE)");
+
+    if (zona == 1) {
+        alert("Envío CABA $250");
+    } else if (zona == 2) {
+        alert("Envío ZONA SUR $450");
+    } else if (zona == 3) {
+        alert("Envío ZONA N0RTE $400");
+    } else if (zona == 4) {
+        alert("Envío CABA $500");
+    }
+}
+
+
